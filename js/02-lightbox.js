84d491b2060281786634e1770e6dc542c2812de5
Vladyslav Shtompel <vladyslav.shtompel@gmail.com>
2022-02-21 14:38:40 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -32,9 +32,8 @@ function handleOpenModal(event) {
     event.preventDefault();
     if (event.target.nodeName !== "IMG") {
     return
-  };
-     
-      
+    };
+    
 galleryModal.on('close.simplelightbox', function () {
 galleryModal.close();
 });

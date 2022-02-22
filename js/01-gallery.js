2a2c4274572357c59883f0fedc425dfdf7aa1ff4
Vladyslav Shtompel <vladyslav.shtompel@gmail.com>
2022-02-22 14:33:41 +0200

    добавлена проверка на отлавливание клика по картинке

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -28,6 +28,10 @@ gallery.addEventListener('click', handleOpenModal);
 
 function handleOpenModal(event) {
     event.preventDefault();
+
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
     modalShow(event.target.dataset.source);
 }
 
